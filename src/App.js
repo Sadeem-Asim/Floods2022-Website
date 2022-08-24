e8aa317fc92f7c90e99077fc64c4699e1926cbf1
Sadeem-Asim <sadeemasim12345@outlook.com>
2022-08-25 03:18:28 +0500

    Connect Button

--- src/App.js
+++ src/App.js
@@ -1,41 +1,30 @@
 import "./App.css";
-// import { useMoralis } from "react-moralis";
-// import { Button } from "web3uikit";
-// import { useEffect } from "react";
+// import Connect from "./components/ConnectButton/connectButton";
+import { ConnectButton } from "web3uikit";
+import { useMoralis } from "react-moralis";
+import { useEffect } from "react";
 
+// import { useEffect } from "react";
 function App() {
-  // const { isAuthenticated, chainId, connector, Moralis } = useMoralis();
-  // console.log(isAuthenticated, chainId, connector);
-  const login = async () => {
-    console.log("loginClicked");
-    // const user = await Moralis.Web3.authenticate();
-    // if (!user) {
-    //   console.log("No User");
-    // }
-  };
-  login();
+  const { chainId, account, isAuthenticated } = useMoralis();
+  console.log(chainId, account, isAuthenticated);
+
+  useEffect(() => {
+    const switchNetwork = () => {
+      if (isAuthenticated) {
+        if (chainId === "0x61") {
+        } else {
+          alert("Change Your Network");
+        }
+      }
+    };
+    switchNetwork();
+  }, [account, chainId]);
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          console.log("Clicked");
-        }}
-      >
-        Hello
-      </button>
+      <ConnectButton />
     </div>
   );
 }
 
 export default App;
-// {
-//   /* <Blockie spotColor="red" seed={account} />
-//       <CryptoCards
-//         chain="polygon"
-//         bgColor="blue"
-//         chainType="Testnet"
-//         onClick={(data) => {
-//           console.log("Data : ", data);
-//         }}
-//       /> */
-// }
