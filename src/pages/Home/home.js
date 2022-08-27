feccbb58fd0e3681af3dee3d71d4831819f438d3
sadeem <sadeemasim12345@outlook.com>
2022-08-28 02:14:24 +0500

    working on second section

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,7 +1,10 @@
 import "./home.scss";
 import Header from "../../layout/header/header";
 import DButton from "./../../components/3dButton/3dButton.jsx";
-import { MDBContainer } from "mdb-react-ui-kit";
+import CompositionImages from "../../components/compositionImages/compositionImages";
+import { MDBContainer, MDBTypography, MDBRow, MDBCol } from "mdb-react-ui-kit";
+import "./home.scss";
+
 const Home = () => {
   return (
     <div className="home">
@@ -15,13 +18,12 @@ const Home = () => {
           muted
           loop
         />
-
         <MDBContainer>
           <div className="banner">
-            {/* <img src="/Images/logo.png" alt="logo" className="logo" /> */}
+            <img src="/Images/logo.png" alt="logo" className="logo" />
             <span className="bannerText">
               <span className="bannerText-1">
-                Mint - Stake Nfts & Enjoy Lifetime Reward{" "}
+                Mint - Stake Nfts & Enjoy Lifetime Reward
               </span>
               <br />
               <span className="bannerText-2">
@@ -33,7 +35,35 @@ const Home = () => {
         </MDBContainer>
       </section>
       <section className="showcaseSection">
-        <MDBContainer></MDBContainer>
+        <MDBContainer>
+          <MDBTypography variant="h1" className="title heading-secondary">
+            lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet
+          </MDBTypography>
+          <MDBRow>
+            <MDBCol md="4" className="left-side">
+              <h3 class="heading-tertiary u-margin-bottom-small">
+                Lorem ipsum dolor sit amet consectetur
+              </h3>
+              <p class="paragraph u-margin-bottom-small">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus alias dicta nobis assumenda illo ipsam, modi nemo amet
+                odio neque, vitae error earum deleniti fugiat quos, voluptatem
+                nesciunt laudantium officia.
+              </p>
+              <h3 class="heading-tertiary u-margin-bottom-small">
+                Lorem ipsum dolor sit amet
+              </h3>
+              <p class="paragraph u-margin-bottom-small">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus alias dicta nobis assumenda illo ipsam, modi nemo amet
+                odio neque,
+              </p>
+            </MDBCol>
+            <MDBCol md="8" className="right-side">
+              <CompositionImages />
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
       </section>
     </div>
   );
