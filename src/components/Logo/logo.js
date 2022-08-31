08e62830bc7210651736dc568405d5de45aa3901
sadeem <sadeemasim12345@outlook.com>
2022-09-01 00:20:35 +0500

    initial commit

--- src/components/Logo/logo.js
+++
@@ -1,9 +0,0 @@
-import "./logo.scss";
-// import { MDBTypography } from "mdb-react-ui-kit";
-import logo from "./../../assets/Images/logo-2.png";
-const Logo = () => (
-  <div>
-    <img src={logo} alt="logo" className="logoHeader" />
-  </div>
-);
-export default Logo;
